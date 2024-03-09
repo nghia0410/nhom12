b3e19a6dd27a83419277dc921ed0cff407df0e4b
nghia0410 <nghiabh20@uef.edu.vn>
2024-03-09 22:27:04 +0700

    setup

---
+++ src/main/java/com/restaurantmanagement/nhom12/Nhom12Application.java
@@ -0,0 +1,13 @@
+package com.restaurantmanagement.nhom12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nhom12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nhom12Application.class, args);
+	}
+
+}
